eed5a7e0497b7e397cd2efd07b179e0c301488db
kiudev <dani.97.saavedra@gmail.com>
2024-12-29 18:29:26 +0100

    create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import { iconFile } from "@/assets/icons";
+import { useTheme } from "@/context/ThemeContext";
+
+export default function Header() {
+  const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <header className="animate-fadeInUp pt-14">
+      <nav className="flex flex-row justify-between items-center">
+        <h1 className="font-nunito text-4xl uppercase tracking-widest font-bold text-color-200 dark:text-color-300">
+          Webpix
+        </h1>
+
+        <button
+          onClick={toggleTheme}
+          className="-mt-1 text-color-200 dark:text-color-300"
+        >
+          {theme === "light"
+            ? iconFile[2].lightModeIcon
+            : iconFile[3].darkModeIcon}
+        </button>
+      </nav>
+    </header>
+  );
+}
