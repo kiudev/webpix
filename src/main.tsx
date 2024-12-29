e27d1a47dd6f824dff129f4420287a4758a515ff
kiudev <dani.97.saavedra@gmail.com>
2024-12-29 12:54:19 +0100

    import file provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { FileProvider } from "./context/FileContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <FileProvider>
+        <App />
+      </FileProvider>
     </ThemeProvider>
   </StrictMode>
 );
