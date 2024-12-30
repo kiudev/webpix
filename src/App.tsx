79a05f8fefc2edc55ec9ce4215ceb1350db34b4c
kiudev <dani.97.saavedra@gmail.com>
2024-12-30 18:37:21 +0100

    nest file provider to the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Welcome from './pages/Welcome'
-import Editor from './pages/Editor'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Welcome from "./pages/Welcome";
+import Editor from "./pages/Editor";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import { FileProvider } from "./context/FileContext";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path='/editor' element={<Editor />} />
-      </Routes>
+      <FileProvider>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/editor" element={<Editor />} />
+          </Route>
+        </Routes>
+      </FileProvider>
     </BrowserRouter>
-  )
+  );
 }
