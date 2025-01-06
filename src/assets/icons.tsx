2084b19a9df59da15846c300cc7674f0499a7548
kiudev <dani.97.saavedra@gmail.com>
2025-01-06 21:19:27 +0100

    add chevron icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -143,4 +143,40 @@ export const iconFile = {
       <path d="M8 9l4 -4" />
     </svg>
   ),
+  chevronsDownIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-chevrons-down"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M7 7l5 5l5 -5" />
+      <path d="M7 13l5 5l5 -5" />
+    </svg>
+  ),
+  chevronsUpIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-chevrons-up"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M7 11l5 -5l5 5" />
+      <path d="M7 17l5 -5l5 5" />
+    </svg>
+  ),
 };
