627991ab9df18886fb267d09d3a0ce5dcd2ae45c
kiudev <dani.97.saavedra@gmail.com>
2025-01-06 19:44:00 +0100

    add new icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,99 +1,146 @@
-export const iconFile = [
-  {
-    imageIcon: (
-      <svg
-        width="180"
-        height="180"
-        viewBox="0 0 180 180"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-32"
-      >
-        <path
-          d="M66.0975 87.09C69.525 84.24 72.9825 84.24 76.4925 87.1725L77.3025 87.9075L114.698 125.303L115.403 125.925C116.846 127.044 118.647 127.597 120.469 127.482C122.292 127.367 124.009 126.592 125.301 125.301C126.592 124.009 127.367 122.292 127.482 120.469C127.597 118.647 127.044 116.846 125.925 115.403L125.303 114.698L115.612 105L117.802 102.802L118.597 102.09C122.025 99.24 125.482 99.24 128.992 102.173L129.802 102.907L164.858 137.97C164.147 145.112 160.9 151.763 155.706 156.715C150.511 161.668 143.713 164.594 136.545 164.963L135 165H45C37.5593 165 30.3843 162.234 24.8682 157.24C19.3521 152.247 15.8885 145.381 15.15 137.977L65.3025 87.8025L66.0975 87.09ZM135 15C142.697 15 150.099 17.9583 155.676 23.2629C161.253 28.5676 164.578 35.8128 164.963 43.5L165 45V116.888L140.303 92.1975L139.177 91.17C129.757 82.9575 117.803 82.9425 108.458 91.0425L107.303 92.0925L105 94.3875L87.8025 77.1975L86.6775 76.17C77.2575 67.9575 65.3025 67.9425 55.9575 76.0425L54.8025 77.0925L15 116.888V45C15 37.3032 17.9583 29.9008 23.2629 24.3239C28.5676 18.747 35.8128 15.4223 43.5 15.0375L45 15H135ZM112.575 52.5L111.623 52.5525C109.8 52.7693 108.12 53.6472 106.901 55.0197C105.682 56.3923 105.008 58.1643 105.008 60C105.008 61.8357 105.682 63.6077 106.901 64.9803C108.12 66.3528 109.8 67.2307 111.623 67.4475L112.5 67.5L113.452 67.4475C115.275 67.2307 116.955 66.3528 118.174 64.9803C119.393 63.6077 120.067 61.8357 120.067 60C120.067 58.1643 119.393 56.3923 118.174 55.0197C116.955 53.6472 115.275 52.7693 113.452 52.5525L112.575 52.5Z"
-          fill="currentColor"
-        />
-      </svg>
-    ),
-  },
-  {
-    heartIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
+export const iconFile = {
+  imageIcon: (
+    <svg
+      width="180"
+      height="180"
+      viewBox="0 0 180 180"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-32"
+    >
+      <path
+        d="M66.0975 87.09C69.525 84.24 72.9825 84.24 76.4925 87.1725L77.3025 87.9075L114.698 125.303L115.403 125.925C116.846 127.044 118.647 127.597 120.469 127.482C122.292 127.367 124.009 126.592 125.301 125.301C126.592 124.009 127.367 122.292 127.482 120.469C127.597 118.647 127.044 116.846 125.925 115.403L125.303 114.698L115.612 105L117.802 102.802L118.597 102.09C122.025 99.24 125.482 99.24 128.992 102.173L129.802 102.907L164.858 137.97C164.147 145.112 160.9 151.763 155.706 156.715C150.511 161.668 143.713 164.594 136.545 164.963L135 165H45C37.5593 165 30.3843 162.234 24.8682 157.24C19.3521 152.247 15.8885 145.381 15.15 137.977L65.3025 87.8025L66.0975 87.09ZM135 15C142.697 15 150.099 17.9583 155.676 23.2629C161.253 28.5676 164.578 35.8128 164.963 43.5L165 45V116.888L140.303 92.1975L139.177 91.17C129.757 82.9575 117.803 82.9425 108.458 91.0425L107.303 92.0925L105 94.3875L87.8025 77.1975L86.6775 76.17C77.2575 67.9575 65.3025 67.9425 55.9575 76.0425L54.8025 77.0925L15 116.888V45C15 37.3032 17.9583 29.9008 23.2629 24.3239C28.5676 18.747 35.8128 15.4223 43.5 15.0375L45 15H135ZM112.575 52.5L111.623 52.5525C109.8 52.7693 108.12 53.6472 106.901 55.0197C105.682 56.3923 105.008 58.1643 105.008 60C105.008 61.8357 105.682 63.6077 106.901 64.9803C108.12 66.3528 109.8 67.2307 111.623 67.4475L112.5 67.5L113.452 67.4475C115.275 67.2307 116.955 66.3528 118.174 64.9803C119.393 63.6077 120.067 61.8357 120.067 60C120.067 58.1643 119.393 56.3923 118.174 55.0197C116.955 53.6472 115.275 52.7693 113.452 52.5525L112.575 52.5Z"
         fill="currentColor"
-        className="icon icon-tabler icons-tabler-filled icon-tabler-heart"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
-      </svg>
-    ),
-  },
-  {
-    lightModeIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="icon icon-tabler icons-tabler-outline icon-tabler-brightness-up w-8 h-8"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-        <path d="M12 5l0 -2" />
-        <path d="M17 7l1.4 -1.4" />
-        <path d="M19 12l2 0" />
-        <path d="M17 17l1.4 1.4" />
-        <path d="M12 19l0 2" />
-        <path d="M7 17l-1.4 1.4" />
-        <path d="M6 12l-2 0" />
-        <path d="M7 7l-1.4 -1.4" />
-      </svg>
-    ),
-  },
-  {
-    darkModeIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="icon icon-tabler icons-tabler-outline icon-tabler-moon w-8 h-8"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-      </svg>
-    ),
-  },
-  {
-    githubIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-      </svg>
-    ),
-  },
-];
+      />
+    </svg>
+  ),
+  heartIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="icon icon-tabler icons-tabler-filled icon-tabler-heart"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
+    </svg>
+  ),
+  lightModeIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-brightness-up w-8 h-8"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M12 5l0 -2" />
+      <path d="M17 7l1.4 -1.4" />
+      <path d="M19 12l2 0" />
+      <path d="M17 17l1.4 1.4" />
+      <path d="M12 19l0 2" />
+      <path d="M7 17l-1.4 1.4" />
+      <path d="M6 12l-2 0" />
+      <path d="M7 7l-1.4 -1.4" />
+    </svg>
+  ),
+  darkModeIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-moon w-8 h-8"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+    </svg>
+  ),
+  githubIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+    </svg>
+  ),
+  downloadIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-download"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+      <path d="M7 11l5 5l5 -5" />
+      <path d="M12 4l0 12" />
+    </svg>
+  ),
+  arrowNarrowDownIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-down"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5l0 14" />
+      <path d="M16 15l-4 4" />
+      <path d="M8 15l4 4" />
+    </svg>
+  ),
+  arrowNarrowUpIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-up"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5l0 14" />
+      <path d="M16 9l-4 -4" />
+      <path d="M8 9l4 -4" />
+    </svg>
+  ),
+};
