c193792796c41d14c49b98cbdf444f394aa0a552
kiudev <dani.97.saavedra@gmail.com>
2025-01-26 19:09:17 +0100

    add bg radial

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,11 +4,13 @@ import Footer from "@/components/footer/Footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen max-w-screen bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100 transition-colors duration-500">
-      <section className="grid grid-flow-row min-h-screen xl:w-[1300px] px-5 md:px-10 lg:m-auto">
+    <div className="min-h-screen max-w-screen bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100">
+      <div className="absolute inset-0 w-full h-full bg-radial-[at_50%] from-primary-500/10 via-primary-500/5 to-neutral-100 dark:bg-radial-[at_50%] dark:from-primary-500/10 dark:via-primary-500/5 dark:to-neutral-900 to-100%"></div>
+
+      <section className="grid grid-flow-row min-h-screen xl:w-[1300px] px-5 md:px-10 lg:m-auto relative">
         <Header />
         {children}
-      <Footer />
+        <Footer />
       </section>
     </div>
   );
