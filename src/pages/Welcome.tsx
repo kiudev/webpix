008ef22a7e762b49a8bbfad44524ff30d085c939
kiudev <dani.97.saavedra@gmail.com>
2025-01-27 13:02:13 +0100

    update home ui

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,12 +5,12 @@ export default function Welcome() {
   return (
     <Layout>
       <main className="flex flex-col mt-0 lg:items-center justify-center gap-10 animate-(--fade-in-up)">
-        <section className="flex flex-col gap-3 font-nunito">
-          <h1 className=" text-2xl md:text-3xl lg:text-6xl font-semibold">
+        <section className="flex flex-col justify-center items-center gap-5 w-full">
+          <h1 className=" text-2xl md:text-3xl lg:text-6xl font-medium">
             Compress your image to{" "}
             <span className="dark:text-primary-500 text-primary-500">WEBP</span>
           </h1>
-          <h2 className="text-sm md:text-md lg:text-2xl text-center">
+          <h2 className="text-sm md:text-md lg:text-xl text-center">
             Optimize your web applications better
           </h2>
           <p className="text-balance hidden md:text-md md:visible">
@@ -18,8 +18,8 @@ export default function Welcome() {
             can enhance loading times and improve overall user experience on
             your website.
           </p>
-        </section>
         <Dropzone />
+        </section>
       </main>
     </Layout>
   );
