248c0a37d346067e9f354fb53f31dc17f7f6b732
kiudev <dani.97.saavedra@gmail.com>
2025-01-27 23:36:18 +0100

    add language provider to the main app

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Welcome from "./pages/Welcome";
 import Editor from "./pages/Editor";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import { FileProvider } from "./context/FileContext";
+import { LanguageProvider } from "./context/LanguageContext";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <FileProvider>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/editor" element={<Editor />} />
-          </Route>
-        </Routes>
-      </FileProvider>
+      <LanguageProvider>
+        <FileProvider>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/editor" element={<Editor />} />
+            </Route>
+          </Routes>
+        </FileProvider>
+      </LanguageProvider>
     </BrowserRouter>
   );
 }
