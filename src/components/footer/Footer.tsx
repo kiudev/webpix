eb45f5bdeb7a3e8a5cb762fb4d2e2943facd6b1d
kiudev <dani.97.saavedra@gmail.com>
2025-01-29 20:09:36 +0100

    add socials

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,12 +2,29 @@ import { iconFile } from "@/assets/icons";
 
 export default function Footer() {
   return (
-    <footer className="pt-40 relative flex justify-center">
-      <p className="absolute bottom-2 flex flex-row gap-1">
-        Made with{" "}
-        <span className="text-primary-500">{iconFile.heartIcon}</span> by
-        Daniel Saavedra
-      </p>
+    <footer className="pt-40 relative">
+      <div className="absolute bottom-5 left-0 right-0 flex flex-row justify-between gap-5 items-center">
+        <p className="flex flex-row gap-1 font-semibold">
+          &copy; {new Date().getFullYear()} Daniel Saavedra
+        </p>
+        <ul className="flex gap-3 items-center">
+          <li className="hover:scale-110 transition-all">
+            <a target="_blank" href="https://github.com/kiudev">
+              {iconFile.githubIcon}
+            </a>
+          </li>
+          <li className="hover:scale-110 transition-all">
+            <a target="_blank" href="https://linkedin.com/in/danisaavedraes/">
+              {iconFile.linkedinIcon}
+            </a>
+          </li>
+          <li className="hover:scale-110 transition-all">
+            <a target="_blank" href="https://danielsaavedra.dev">
+              {iconFile.portfolioIcon}
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
