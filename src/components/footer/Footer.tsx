d84b6049120a97df8d2215034ff0dff7c01e8cc8
kiudev <dani.97.saavedra@gmail.com>
2025-01-30 11:51:13 +0100

    center footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,10 +3,7 @@ import { iconFile } from "@/assets/icons";
 export default function Footer() {
   return (
     <footer className="pt-40 relative">
-      <div className="absolute bottom-5 left-0 right-0 flex flex-row justify-between gap-5 items-center">
-        <p className="flex flex-row gap-1 font-semibold">
-          &copy; {new Date().getFullYear()} Daniel Saavedra
-        </p>
+      <div className="absolute bottom-5 left-0 right-0 flex flex-row justify-center gap-5 items-center">
         <ul className="flex gap-3 items-center">
           <li className="hover:scale-110 transition-all">
             <a target="_blank" href="https://github.com/kiudev">
@@ -24,6 +21,10 @@ export default function Footer() {
             </a>
           </li>
         </ul>
+        <hr className="bg-neutral-900 dark:bg-neutral-100 w-0.5 h-5" />
+        <p className="flex flex-row gap-1">
+          &copy; {new Date().getFullYear()} Daniel Saavedra
+        </p>
       </div>
     </footer>
   );
